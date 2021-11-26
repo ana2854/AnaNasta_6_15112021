f1e23cd5ef7891d674e85ea1874ce6f46324395c
Ana Nasta <univpro81@gmail.com>
2021-11-26 08:00:54 +0100

    gestions erreurs de cors

--- backend/app.js
+++ backend/app.js
@@ -4,23 +4,38 @@ const express = require('express');
 
 const app = express();
 
+//le CORS définit comment les serveurs et les navigateurs interagissent en spécifiant quelles ressources peuvent être demandées 
+//app.use permet d'accéder d'attribuer un middleware à une route spécifique de mon app
+//middleware avec des headers permettant d'accéder à notre api depuis n'importe quelle origine
+//+ headers permettant d'ajouter des headers mentionnés aux requêtes envoyées vers notre API
+//+ headers permettant d'envoyer des requêtes avec les méthodes mentionnées GET POST
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
-});
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+app.use('/api/stuff', (req, res, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    res.status(200).json(stuff);
+  });
 
 module.exports = app;
